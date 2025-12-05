b6ba8dd2cc2a9d30dda80ea65cd6432d5a15f3bb
CryptoTechSXM <CryptoCounsels@gmail.com>
2025-12-05 11:01:21 -0400

    Create TrueVaultTimelock.sol

---
+++ contracts/TrueVaultTimelock.sol
@@ -0,0 +1,8 @@
+ // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import "@openzeppelin/contracts/governance/TimelockController.sol";
+
+contract TrueVaultTimelock is TimelockController {
+    constructor(uint256 d, address[] memory p, address[] memory e, address a) TimelockController(d,p,e,a) {}
+}
