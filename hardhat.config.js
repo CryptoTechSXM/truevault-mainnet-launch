54d9aa905e3ab800630221dae130351cff36f03f
CryptoTechSXM <CryptoCounsels@gmail.com>
2025-12-05 11:07:55 -0400

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,1 +1,12 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
+module.exports = {
+  solidity: "0.8.26",
+  networks: {
+    bsc: {
+      url: "https://bsc-dataseed.binance.org/",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
+    }
+  }
+};
